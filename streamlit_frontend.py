2c72eb9ef3346cfef8b6b912c9946a372cff4d24
Shandesh Chaudhary <hancyshandeshc@gmail.com>
2026-09-11 16:58:02 +0545

    Update streamlit_frontend.py

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -25,7 +25,7 @@ if user_input:
 
     response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
  
-    ai_message = response['messages'][-1].content
+    ai_message = response['messages'][-1].text
     # first add the message to message_history
     st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
     with st.chat_message('assistant'):
